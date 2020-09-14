7ac198c9a77835931a778c0a8d084e18d4c03f3a
dohpkim <dohp.kim@gmail.com>
2020-09-14 09:11:11 +0900

    WIP: address checksum EIP55

--- src/main/java/web3jv/wallet/Wallet.java
+++ src/main/java/web3jv/wallet/Wallet.java
@@ -4,51 +4,66 @@ import org.bouncycastle.jcajce.provider.digest.Keccak;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
 import org.bouncycastle.util.encoders.Hex;
 
 import java.math.BigInteger;
-import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class Wallet {
 
-    public static void main(String[] args) throws Exception {
-        getAddress("fdd8b595a0ba6ba01c04ab62bd1ab0fb6464fe44a3f32ac07ef236298e97e1aa");
+    public static void main(String[] args) {
+        String priKey = "fdd8b595a0ba6ba01c04ab62bd1ab0fb6464fe44a3f32ac07ef236298e97e1aa";
+        System.out.println(checkAddressEIP55("0x9fab8c7a16b7cbaa1cf2b8568e8d1b3a59aeb827"));
 
     }
 
     public static String generatePrivateKey() {
         Random random = new Random();
-        String privateKey = "";
+        StringBuilder privateKey = new StringBuilder();
         for (int i = 0; i < 64; i++) {
-            privateKey += Integer.toHexString(random.nextInt(16));
+            privateKey.append(Integer.toHexString(random.nextInt(16)));
         }
 
-        return privateKey;
+        return privateKey.toString();
     }
 
-    public static String getAddress(String priKey) throws Exception {
-
+    public static String getPublicKey(String priKey) {
+        byte[] priByte = new BigInteger(priKey, 16).toByteArray(); // it adds 00 padding at the beginning
         ECNamedCurveParameterSpec params = ECNamedCurveTable.getParameterSpec("secp256k1");
-        ECPoint pointQ = params.getG().multiply(new BigInteger(1, new BigInteger(priKey, 16).toByteArray()));
-        String publicKey = Hex.toHexString(pointQ.getEncoded(false));
-        String key = publicKey.substring(2);
+        ECPoint pointQ = params.getG().multiply(new BigInteger(priByte));
 
-        Keccak.Digest256 digest256 = new Keccak.Digest256();
-        digest256.update(new BigInteger(key, 16).toByteArray());
-        byte[] keccakHash = digest256.digest(new BigInteger(key, 16).toByteArray());
-        String address1 = "0x" + Hex.toHexString(keccakHash).substring(24);
-
-        MessageDigest digest = MessageDigest.getInstance("SHA-256"); // Keccak
-        byte[] hash = digest.digest(new BigInteger(key, 16).toByteArray());
-        String keccacked = Hex.toHexString(hash);
-        String address = "0x" + keccacked.substring(24);
-
-        System.out.println(keccacked);
-        System.out.println(address);
-        String and = "9Fab8C7A16B7cBAA1cF2B8568e8D1b3A59AEb827";
-
-        return address1;
+        return Hex.toHexString(pointQ.getEncoded(false)).substring(2);
     }
-}
 
+    public static String getKeccack256HexString(String publicKey) {
+        Keccak.DigestKeccak keccak = new Keccak.Digest256();
+        byte[] bytes = ByteUtils.fromHexString(publicKey);
+        String hexed = Hex.toHexString(keccak.digest(bytes));
+
+        return hexed.substring(hexed.length() - 40);
+    }
+
+    public static String getAddress(String pubKey) {
+        return "0x" + getKeccack256HexString(pubKey);
+    }
+
+    public static boolean checkAddressEIP55(String address) {
+        String target = address.startsWith("0x") ? address.substring(2) : address;
+        String lower = target.toLowerCase();
+        char[] checksum = getKeccack256HexString(lower).toCharArray();
+        System.out.println(getKeccack256HexString(lower));
+
+        char[] subject = target.toCharArray();
+        for (int i = 0; i < subject.length; i++) {
+            if (Character.isUpperCase(subject[i])) {
+                return checksum[i] >= 8 || Character.isLetter(checksum[i]);
+            }
+        }
+
+        return false;
+    }
+
+}
--- src/test/java/web3jv/wallet/TestWallet.java
+++ src/test/java/web3jv/wallet/TestWallet.java
@@ -1,17 +1,39 @@
 package web3jv.wallet;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestWallet {
 
     @Test
-    public void generatePrivKeyAndAddress() throws Exception {
+    public void generateAddress() {
 
         String privateKey = "fdd8b595a0ba6ba01c04ab62bd1ab0fb6464fe44a3f32ac07ef236298e97e1aa";
-        String address = Wallet.getAddress(privateKey);
+        String address = Wallet.getAddress(Wallet.getPublicKey(privateKey));
 
         assertEquals("0x9Fab8C7A16B7cBAA1cF2B8568e8D1b3A59AEb827", address);
+    }
+
+    @DisplayName("개인키 입력시 공개키 생성되는지 테스트")
+    @Test
+    public void generatePublicKeyFromPrivateKey() {
+
+        String SamplePriKey = "18dd1dcd752466afa3d1fac1424333c6461c3a0f1d6702e9c45bc9254ec74e5f";
+        String result = Wallet.getPublicKey(SamplePriKey);
+
+        assertEquals("bdfb71e2d953406c45279ac434667a6a1ea9fae608af91e7f6bfb0792011df760895a528e8b8362288" +
+                "6039b4803b6182d708fb40a16919bddaef84493ef1d4cf", result);
+    }
+
+    @Test
+    public void addressChecksumEIP55Test() {
+
+        String addressTest = "0x9Fab8C7A16B7cBAA1cF2B8568e8D1b3A59AEb827";
+        boolean result = Wallet.checkAddressEIP55(addressTest);
+
+        assertTrue(result);
     }
 }
