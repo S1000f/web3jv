413c8f3e018bf80a46a39ba83ffd79aacbcf9a61
nick <dohp.kim@coinvest.kr>
2020-09-16 09:53:28 +0900

    toWei, fromWei BigDecimal 연산으로 변경

--- src/main/java/web3jv/utils/Utils.java
+++ src/main/java/web3jv/utils/Utils.java
@@ -5,58 +5,27 @@ import java.math.RoundingMode;
 
 public class Utils {
 
+    public static String fromWei(BigDecimal amount, EtherUnit targetUnit) {
+        BigDecimal position = new BigDecimal("10").pow(targetUnit.getWeiValue());
+        amount = amount.divide(position, targetUnit.getWeiValue(), RoundingMode.CEILING);
 
-//    public static String fromWei(String amount, EtherUnit targetUnit) {
-//        StringBuilder builder = new StringBuilder(amount);
-//        int pos = targetUnit.getWeiValue();
-//        int offset = amount.length() - pos;
-//        if (offset > 0) {
-//            builder.insert(offset, '.');
-//        } else {
-//            for (int i = 0; i < offset * -1; i++) {
-//                builder.insert(0, '0');
-//            }
-//            builder.insert(0, "0.");
-//        }
-//
-//        return builder.toString();
-//    }
-
-    public static String fromWei(String amount, EtherUnit targetUnit) {
-        BigDecimal asd = new BigDecimal(amount);
-        System.out.println(asd.toString());
-
-        BigDecimal d = new BigDecimal("10").pow(targetUnit.getWeiValue());
-        System.out.println(d.toString());
-
-        asd = asd.divide(d, targetUnit.getWeiValue(), RoundingMode.CEILING);
-
-        return asd.toString();
+        return amount.toString();
     }
 
-//    public static String toWei(String amount, EtherUnit inputUnit) {
-//        StringBuilder builder = new StringBuilder(amount);
-//        int pos = inputUnit.getWeiValue();
-//        int dotIndex = amount.indexOf(".");
-//        int lengthAfterDot = amount.length() - (dotIndex + 1);
-//        int offset = pos - lengthAfterDot;
-//        if (offset > 0) {
-//            for (int i = 0; i < offset; i++) {
-//                builder.append("0");
-//            }
-//        } else if (offset < 0) {
-//            builder.insert(dotIndex + (offset * -1) + 1, ".");
-//        }
-////        if (dotIndex != null) {
-////            builder.deleteCharAt(dotIndex);
-////            String balance = builder.toString();
-////            while (balance.startsWith("0")) {
-////                balance = balance.substring(1);
-////            }
-////
-////        }
-//
-////        return balance;
-//    }
+    public static String fromWei(String amount, EtherUnit targetUnit) {
+        BigDecimal converted = new BigDecimal(amount);
+        return fromWei(converted, targetUnit);
+    }
 
+    public static String toWei(BigDecimal amount, EtherUnit originUnit) {
+        BigDecimal position = new BigDecimal("10").pow(originUnit.getWeiValue());
+        amount = amount.multiply(position).stripTrailingZeros();
+
+        return amount.toPlainString();
+    }
+
+    public static String toWei(String amount, EtherUnit originUnit) {
+        BigDecimal converted = new BigDecimal(amount);
+        return toWei(converted, originUnit);
+    }
 }
--- src/test/java/web3jv/utils/TestUtils.java
+++ src/test/java/web3jv/utils/TestUtils.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import web3jv.jsonrpc.Web3jv;
 
-import java.math.BigInteger;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUtils {
@@ -21,63 +19,59 @@ public class TestUtils {
     @DisplayName("18자리 wei를 ether로 변환시 0.x...가 된다")
     @Test
     public void fromWeiToEtherLength18Test() {
-        BigInteger balanceWei = new BigInteger("545109094442210255");
-        String result = Utils.fromWei(balanceWei.toString(), EtherUnit.ETHER);
+        String result = Utils.fromWei("545109094442210255", EtherUnit.ETHER);
 
         assertEquals("0.545109094442210255", result);
     }
 
-//    @DisplayName("16자리 wei를 ether로 변환시 0.00x...가 된다")
-//    @Test
-//    public void fromWeiToEtherLengthUnder18Test() {
-//        BigInteger balanceWei = new BigInteger("5451090944422102");
-//        String result = Utils.fromWei(balanceWei, EtherUnit.ETHER);
-//
-//        assertEquals("0.005451090944422102", result);
-//    }
-//
-//    @DisplayName("20자리 wei를 ether로 변환 시 xx.x...로 된다")
-//    @Test
-//    public void fromWeiToEtherLengthOver18Test() {
-//        BigInteger balanceWei = new BigInteger("50010909444221025577");
-//        String result = Utils.fromWei(balanceWei, EtherUnit.ETHER);
-//
-//        assertEquals("50.010909444221025577", result);
-//    }
-//
-//    @DisplayName("wei를 Gwei로 변환된다")
-//    @Test
-//    public void fromWeiToGewiTest() {
-//        BigInteger balanceWei = new BigInteger("545109094442210255");
-//        String result = Utils.fromWei(balanceWei, EtherUnit.GWEI);
-//
-//        assertEquals("545109094.442210255", result);
-//    }
-//
-//    @DisplayName("Ether단위가 wei단위로 변환된다")
-//    @Test
-//    public void toWeiFromEtherTest() {
-//        String result = Utils.toWei("0.545109094442210255", EtherUnit.ETHER);
-//        String result2 = Utils.toWei("0.005451090944422102", EtherUnit.ETHER);
-//        String result3 = Utils.toWei("50.010909444221025577", EtherUnit.ETHER);
-//        String result4 = Utils.toWei("52.01090", EtherUnit.ETHER);
-//        String result5 = Utils.toWei("24", EtherUnit.ETHER);
-//
-//        assertEquals("545109094442210255", result);
-//        assertEquals("5451090944422102", result2);
-//        assertEquals("50010909444221025577", result3);
-//        assertEquals("52010900000000000000", result4);
-//        assertEquals("24000000000000000000", result5);
-//    }
-//
-//    @Test
-//    public void toWeiFromMicroetherTest() {
-//        String result = Utils.toWei("0.545109094442210255", EtherUnit.GWEI);
-//        String result2 = Utils.toWei("35666", EtherUnit.GWEI);
-//
-//        assertEquals("545109094.442210255", result);
-//        assertEquals("35666000000000", result2);
-//    }
+    @DisplayName("16자리 wei를 ether로 변환시 0.00x...가 된다")
+    @Test
+    public void fromWeiToEtherLengthUnder18Test() {
+        String result = Utils.fromWei("5451090944422102", EtherUnit.ETHER);
+
+        assertEquals("0.005451090944422102", result);
+    }
+
+    @DisplayName("20자리 wei를 ether로 변환 시 xx.x...로 된다")
+    @Test
+    public void fromWeiToEtherLengthOver18Test() {
+        String result = Utils.fromWei("50010909444221025577", EtherUnit.ETHER);
+
+        assertEquals("50.010909444221025577", result);
+    }
+
+    @DisplayName("wei를 Gwei로 변환된다")
+    @Test
+    public void fromWeiToGewiTest() {
+        String result = Utils.fromWei("545109094442210255", EtherUnit.GWEI);
+
+        assertEquals("545109094.442210255", result);
+    }
+
+    @DisplayName("Ether단위가 wei단위로 변환된다")
+    @Test
+    public void toWeiFromEtherTest() {
+        String result = Utils.toWei("0.545109094442210255", EtherUnit.ETHER);
+        String result2 = Utils.toWei("0.005451090944422102", EtherUnit.ETHER);
+        String result3 = Utils.toWei("50.010909444221025577", EtherUnit.ETHER);
+        String result4 = Utils.toWei("52.01090", EtherUnit.ETHER);
+        String result5 = Utils.toWei("24", EtherUnit.ETHER);
+
+        assertEquals("545109094442210255", result);
+        assertEquals("5451090944422102", result2);
+        assertEquals("50010909444221025577", result3);
+        assertEquals("52010900000000000000", result4);
+        assertEquals("24000000000000000000", result5);
+    }
+
+    @Test
+    public void toWeiFromMicroetherTest() {
+        String result = Utils.toWei("0.545109094442210255", EtherUnit.GWEI);
+        String result2 = Utils.toWei("35666", EtherUnit.GWEI);
+
+        assertEquals("545109094.442210255", result);
+        assertEquals("35666000000000", result2);
+    }
 
 
 
